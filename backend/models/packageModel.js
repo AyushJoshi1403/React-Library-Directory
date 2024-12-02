94c95b62796235f6348d9663c844945bbc551a2f
AyushJoshi1403 <ayush.aj1403@gmail.com>
2024-12-02 16:29:06 +0530

    project

--- backend/models/packageModel.js
+++ backend/models/packageModel.js
@@ -2,12 +2,12 @@ const { model, Schema } = require('../connection');
 
 const mySchema = new Schema({
     title: String,
-    version: String,
     packageName: String,
+    version: String,
+    difficulty: { type: Number, default: 0 },
     downloads: { type: Number, default: 0 },
     publishedBy: String,
     lastPublished: { type: Date, required: true },
-    difficulty: { type: Number, default: 0 },
     link: String,
     createdAt: { type: Date, default: Date.now }
 });
