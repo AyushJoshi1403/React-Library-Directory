097ec24dcf0e68bb55db95852ac6a2849f1a75a2
AyushJoshi1403 <ayush.aj1403@gmail.com>
2024-12-27 21:03:55 +0530

    frontend

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -43,40 +43,94 @@ const Packages = () => {
         <div>
           <div>
             <div>
-              <label>Title</label>
-              <input type="text" />
+              <label
+                className='block text-sm mb-2 dark:text-white'>
+                Title
+              </label>
+              <input
+                type="text"
+                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
+              />
             </div>
             <div>
-              <label>Package Name</label>
-              <input type="text" />
+              <label
+                className='block text-sm mb-2 dark:text-white'>
+                Package Name
+              </label>
+              <input
+                type="text"
+                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
+              />
             </div>
             <div>
-              <label>Version</label>
-              <input type="text" />
+              <label
+                className='block text-sm mb-2 dark:text-white'>
+                Version
+              </label>
+              <input
+                type="text"
+                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
+              />
             </div>
             <div>
-              <label>Last Published</label>
-              <input type="text" />
+              <label
+                className='block text-sm mb-2 dark:text-white'>
+                Last Published
+              </label>
+              <input
+                type="text"
+                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
+              />
             </div>
             <div>
-              <label>Difficulty</label>
-              <input type="text" />
+              <label
+                className='block text-sm mb-2 dark:text-white'>
+                Difficulty
+              </label>
+              <input
+                type="text"
+                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
+              />
             </div>
             <div>
-              <label>Downloads</label>
-              <input type="text" />
+              <label
+                className='block text-sm mb-2 dark:text-white'>
+                Downloads
+              </label>
+              <input
+                type="text"
+                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
+              />
             </div>
             <div>
-              <label>Published By</label>
-              <input type="text" />
+              <label
+                className='block text-sm mb-2 dark:text-white'>
+                Published By
+              </label>
+              <input
+                type="text"
+                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
+              />
             </div>
             <div>
-              <label>Link</label>
-              <input type="text" />
+              <label
+                className='block text-sm mb-2 dark:text-white'>
+                Link
+              </label>
+              <input
+                type="text"
+                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
+              />
             </div>
             <div>
-              <label>Created At</label>
-              <input type="text" />
+              <label
+                className='block text-sm mb-2 dark:text-white'>
+                Created At
+              </label>
+              <input
+                type="text"
+                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
+              />
             </div>
           </div>
         </div>
