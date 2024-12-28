2a97873dd3ab3dab05a34f909d433ef7a610ec18
AyushJoshi1403 <ayush.aj1403@gmail.com>
2024-12-28 14:57:57 +0530

    frontend

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -35,105 +35,43 @@ const Packages = () => {
   }, [])
 
   return (
-    <div>
-      <div>
-        <div>
-          <h1 className='text-center text-2xl font-bold'>List of Packages</h1>
-        </div>
-        <div>
-          <div>
-            <div>
-              <label
-                className='block text-sm mb-2 dark:text-white'>
-                Title
-              </label>
-              <input
-                type="text"
-                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
-              />
-            </div>
-            <div>
-              <label
-                className='block text-sm mb-2 dark:text-white'>
-                Package Name
-              </label>
-              <input
-                type="text"
-                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
-              />
-            </div>
-            <div>
-              <label
-                className='block text-sm mb-2 dark:text-white'>
-                Version
-              </label>
-              <input
-                type="text"
-                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
-              />
-            </div>
-            <div>
-              <label
-                className='block text-sm mb-2 dark:text-white'>
-                Last Published
-              </label>
-              <input
-                type="text"
-                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
-              />
-            </div>
-            <div>
-              <label
-                className='block text-sm mb-2 dark:text-white'>
-                Difficulty
-              </label>
-              <input
-                type="text"
-                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
-              />
-            </div>
-            <div>
-              <label
-                className='block text-sm mb-2 dark:text-white'>
-                Downloads
-              </label>
-              <input
-                type="text"
-                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
-              />
-            </div>
-            <div>
-              <label
-                className='block text-sm mb-2 dark:text-white'>
-                Published By
-              </label>
-              <input
-                type="text"
-                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
-              />
-            </div>
-            <div>
-              <label
-                className='block text-sm mb-2 dark:text-white'>
-                Link
-              </label>
-              <input
-                type="text"
-                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
-              />
-            </div>
-            <div>
-              <label
-                className='block text-sm mb-2 dark:text-white'>
-                Created At
-              </label>
-              <input
-                type="text"
-                className='border py-2 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
-              />
-            </div>
-          </div>
-        </div>
+    <div className=''>
+      <div className='container mx-auto py-10'>
+        <h1 className='text-center text-2xl font-bold'>Manage Users</h1>
+
+        <table className='w-full '>
+          <thead>
+            <tr className='bg-gray-600 text-white font-bold'>
+              <th className='p-3'>Title</th>
+              <th className='p-3'>Package Name</th>
+              <th className='p-3'>Version</th>
+              <th className='p-3'>Last Published</th>
+              <th className='p-3'>Difficulty</th>
+              <th className='p-3'>Downloads</th>
+              <th className='p-3'>Published By</th>
+              <th className='p-3'>Link</th>
+              <th className='p-3'>Created At</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              packageList.map((user) => {
+                return (
+                  <tr className='border bg-gray-200'>
+                    <td className='p-3'>{user._id}</td>
+                    <td className='p-3'>{user.name}</td>
+                    <td className='p-3'>{user.email}</td>
+                    <td className='p-3'>{user.password}</td>
+                    <td className='p-3'>{user.city}</td>
+                    <td className='p-3'>{new Date(user.createdAt).toDateString()}</td>
+                    
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+
       </div>
     </div>
   )
