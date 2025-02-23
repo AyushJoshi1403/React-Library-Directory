d438e2e566ee403350686e1bf86b335e771e7beb
AyushJoshi1403 <ayush.aj1403@gmail.com>
2025-02-23 23:20:32 +0530

    project

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div>
       <div>
-        <div className='text-center text-3xl font-bold mt-10'>
+        <div className='text-center text-2xl font-bold mt-10'>
         <h1>React Library Directory</h1>
         </div>
         <div><h1 className='text-center text-2xl'>List of Library</h1></div>
