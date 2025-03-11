60fc5ed52fcb67b20fc1c359adf8fc47ea420407
AyushJoshi1403 <ayush.aj1403@gmail.com>
2025-03-12 04:20:21 +0530

    frontend

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <div>
-        <div className='text-center text-2xl font-bold mt-10'>
-        <h1>React Library Directory</h1>
+      <div className='border bg-gray-900 border-transparent'>
+        <div className='text-center text-3xl font-bold mt-10 mb-6'>
+        <h1>List of librarys</h1>
         </div>
-        <div><h1 className='text-center text-2xl'>List of Library</h1></div>
+        <div className='text-center'>
+          <input className='w-[75%] border bg-transparent rounded-l' type="text" placeholder='Search libraries...' />
+        </div>
       </div>
     </div>
   )
